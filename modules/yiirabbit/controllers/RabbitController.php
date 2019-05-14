73bbde1bfef6d5bb20a19119bd8f4a8ab72563b3
Viktor07071986 <vitek.86@mail.ru>
2019-05-14 22:46:01 +0300

    four commit

--- modules/yiirabbit/controllers/RabbitController.php
+++ modules/yiirabbit/controllers/RabbitController.php
@@ -12,6 +12,19 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class RabbitController extends Controller {
 
+    public $connection;
+    public $channel;
+    public $queue;
+    public $exchange;
+
+    public function init() {
+        $this->queue = "RabbitMQQueue";
+        $this->exchange = "amq.direct";
+        $this->connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $this->channel = $this->connection->channel();
+        parent::init();
+    }
+
     public function actionIndex () {
         return $this->render("rabbitmq");
     }
@@ -19,8 +32,6 @@ class RabbitController extends Controller {
     public function actionReader () {
         $model = new RabbitReaderForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-            $channel = $connection->channel();
             $rabbitForm = Yii::$app->request->post()["RabbitReaderForm"]["count_queue_message"];
             $sample = "<table border='1px solid black;' cellspacing='0' cellpadding='7' class='table table-striped'>
                         <tr>
@@ -30,10 +41,10 @@ class RabbitController extends Controller {
                             <th>Время</th>
                         </tr>";
             for ($i = 0; $i < $rabbitForm; $i++) {
-                $result = ($channel->basic_get('RabbitMQQueue', true, null)->body);
+                $result = ($this->channel->basic_get('RabbitMQQueue', true, null)->body);
                 $rez = json_decode($result, true);
                 if (is_null($rez)) {
-                    $sample .= "<hr/>Доступных сообщений для выгрузки больше нет!";
+                    $sample .= "<h4>Доступных сообщений для выгрузки больше нет!</h4>";
                     break;
                 } else {
                     $sample .= "<tr>
@@ -49,27 +60,28 @@ class RabbitController extends Controller {
         return $this->render("reader", compact('model', 'sample'));
     }
 
-
     public function actionWriter () {
         $model = new RabbitWriterForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             $rabbitForm=Yii::$app->request->post();
             $rabbitForm = $rabbitForm["RabbitWriterForm"];
-            $queue = "RabbitMQQueue";
-            $exchange = "amq.direct";
-            $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest', '/', false, 'AMQPLAIN', null, 'en_US', 30);
-            $channel = $connection->channel();
-            $channel->queue_declare($queue, false, true, false, false);
-            $channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
-            $channel->queue_bind($queue, $exchange);
+            $this->channel->queue_declare($this->queue, false, true, false, false);
+            $this->channel->exchange_declare($this->exchange, AMQPExchangeType::DIRECT, false, true, false);
+            $this->channel->queue_bind($this->queue, $this->exchange);
             $messageBody = json_encode($rabbitForm);
             $message = new AMQPMessage($messageBody, array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
-            $channel->basic_publish($message, $exchange);
-            $channel->close();
-            $connection->close();
+            $this->channel->basic_publish($message, $this->exchange);
+            $this->channel->close();
+            $this->connection->close();
             return $this->redirect('writer');
         }
         return $this->render("writer", compact('model'));
+    }
+
+    public function __destruct() {
+        // TODO: Implement __destruct() method. not working!!!
+        //$this->channel->close();
+        //$this->connection->close();
     }
 
 }
